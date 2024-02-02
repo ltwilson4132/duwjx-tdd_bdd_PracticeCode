af267d93ea601fc169835fdb6af5ca41e150f1f3
Logan Wilson <ltwilson4132@gmail.com>
2024-02-02 14:58:23 -0500

    Lab 10 done. Test data is loaded successfully.

--- labs/10_loading_test_data/features/steps/load_steps.py
+++ labs/10_loading_test_data/features/steps/load_steps.py
@@ -10,4 +10,27 @@ import requests
 from behave import given, when, then
 
 # Load data here
+@given('the following pets')
+def step_impl(context):
+    """Refresh all Pets in the database"""
+    # Deleting all pets
+    response = requests.get(f"{context.base_url}/pets")
+    assert response.status_code == 200
+
+    for pet in response.json():
+        response = requests.delete(f"{context.base_url}/pets/{pet['id']}")
+        assert response.status_code == 204
+
+    # Creating new pets to test with
+    for row in context.table:
+        payload = {
+            'name': row['name'],
+            'category': row['category'],
+            'available': row['available'] in ['True', 'true', '1'],
+            'gender': row['gender'],
+            'birthday': row['birthday']
+        }
+
+        response = requests.post(f"{context.base_url}/pets", json=payload)
+        assert response.status_code == 201
 
